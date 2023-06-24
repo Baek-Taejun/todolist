560e7b321b238fba2046a609d3900240a549e313
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-06-24 11:30:18 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+<<<<<<< HEAD
 function Son(props) {
   //상속받기 위해서 자식인 Son이 props를 넣음.
   console.log(props);
   return (
     <div>
       나는 {props.grandFatherName}의손자{props.motherName}의 딸이에요!
+=======
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+
+function App() {
+  let [todo, setTodo] = useState("제목");
+
+  return (
+    <div className="App">
+      {/* 나브박스 */}
+      <div className="box-nav">
+        <div className="nav-text1">
+          <h4>My Todo List</h4>
+        </div>
+        <div className="nav-text2">
+          <h4>React</h4>
+        </div>
+      </div>
+      {/* 제목,내용박스 */}
+      <div className="list">
+        <div className="input-container">
+          <label className="labelInput1">제목</label>
+          <input className="titleInput1" type="text" />
+          <label className="labelInput2">내용</label>
+          <input className="titleInput2" type="text" />
+        </div>
+        <button className="titleButton">추가하기</button>
+      </div>
+      <Cards></Cards>
+>>>>>>> parent of 73c0007 (finish)
     </div>
   );
 }
 
+<<<<<<< HEAD
 function Mother(props) {
   //상속을 받으려는 자식은 props를 넣어줘야 한다. 그럼저기 16번째줄에 있는게 상속됨.
   console.log(props);
@@ -21,3 +55,23 @@ function GrandFather() {
 }
 
 export default GrandFather; //여기는 최상위 컴포넌트와 명이 같아야 한다.
+=======
+function Cards() {
+  return (
+    <Card className="card-box">
+      <Card.Body>
+        <div>
+          <h4>리액트 공부하기</h4>
+          <p>리액트 기초를 공부해 봅시다.</p>
+        </div>
+        <div>
+          <button>삭제하기</button>
+          <button>완료</button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default App;
+>>>>>>> parent of 73c0007 (finish)
